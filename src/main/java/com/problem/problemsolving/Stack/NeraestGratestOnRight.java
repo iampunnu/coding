57e60b46f71e91f9171d5d2b24d6393085aab1e8
praveen kumar singh <praveen304.ps@gmail.com>
2024-01-25 11:54:18 +0530

    stack change

---
+++ src/main/java/com/problem/problemsolving/Stack/NeraestGratestOnRight.java
@@ -0,0 +1,26 @@
+package com.problem.problemsolving.Stack;
+
+import java.util.Stack;
+
+public class NeraestGratestOnRight {
+    public static void main(String[] args) {
+        int arr[]={5,2,8,10,12,6,1};
+        int []ans= new int[arr.length];
+        java.util.Stack<Integer> stack=new Stack<>();
+        for (int i=arr.length-1;i>=0;i--){
+            while (stack.size()>0 && stack.peek()<=arr[i]){
+                stack.pop();
+            }
+            if (stack.size()==0){
+                ans[i]=-1;
+            }
+            else {
+                ans[i]=stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        for (int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+}
